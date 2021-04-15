c3daff58b2ce980686d7c9b9cb8e50cb8a806d5f
CSKIM999 <75712211+CSKIM999@users.noreply.github.com>
2021-04-15 16:59:09 +0900

    First push

---
+++ TF/tensor.py
@@ -0,0 +1,11 @@
+from numpy.core.fromnumeric import ptp
+import pandas as pd
+import numpy as np
+
+
+data = pd.read_csv('http://bit.ly/ld-sample-idol', index_col='순위')
+print(data)
+print('='*50)
+print(data.head())
+print('='*50)
+print(data.iloc[3])
