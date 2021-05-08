79d44238ab95b2fe5ab7fdb66de7a7f2db6cbff3
CSKIM999 <75712211+CSKIM999@users.noreply.github.com>
2021-05-08 22:28:39 +0900

    21/05/08 22:28

--- Predict_CoinMoving/json_practice.py
+++ Predict_CoinMoving/json_practice.py
@@ -28,4 +28,47 @@ respone = requests.get(addr)
 
 data = respone.json()
 
-print(data)
+# def get_upbit_data(url, last_date, to_date) :
+#     global show_title
+#     global index
+    
+#     fail2GetData = False
+#     failCnt = 0
+#     response = ''
+#     while True:
+#         try :
+#             response = requests.get(url)
+#         except Exception as e:
+#             print(e)
+#             time.sleep(20)
+#             continue
+#         if str(response) == '<Response [200]>':
+#             break
+#         time.sleep(10)
+#     if ( fail2GetData )  :
+#         print("Fail to access url")
+#         exit()
+
+#     data = response.json()
+
+#     code = data[0]['code']
+#     date = ''
+#     dateKst = ''
+
+#     for i in range(len(data))  :
+#         dateKst = data[i]['candleDateTimeKst']
+#         date = data[i]['candleDateTime']
+#         if (last_date == '' or last_date > date) :
+#             if (dateKst >= to_date) :
+#                 simpleDate = dateKst.split('+')
+#                 print(simpleDate[0], "%6d"%data[i]['openingPrice'], "%6d"%data[i]['highPrice'], "%6d"%data[i]['lowPrice'], "%6d"%data[i]['tradePrice'], "%9d"%(data[i]['candleAccTradeVolume']));
+#                 datas = (simpleDate[0], "%6f"%data[i]['openingPrice'], "%6f"%data[i]['highPrice'], "%6f"%data[i]['lowPrice'], "%6f"%data[i]['tradePrice'], "%9f"%(data[i]['candleAccTradeVolume']))
+#                 for seq, name in enumerate(datas):
+#                     sheet.cell(row=index, column=seq+1, value=name)
+
+#             else :
+#                 break
+#         index+=1
+#         # print('still working...')
+
+#     return date
