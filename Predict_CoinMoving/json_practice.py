c2fe7d30cb902d800de320b580d2c99914020937
CSKIM999 <75712211+CSKIM999@users.noreply.github.com>
2021-05-12 17:06:52 +0900

    21/05/12 17:06
    practice data framing

--- Predict_CoinMoving/json_practice.py
+++ Predict_CoinMoving/json_practice.py
@@ -1,8 +1,12 @@
 import json
 import requests
 import urllib.request
+import time
+import pandas as pd
 
-
+df = pd.DataFrame()
+index = 2
+last_date = ''
 
 def get_coin_data_url(coinName, type, scale, cnt=400) :
     addr = 'https://crix-api-endpoint.upbit.com/v1/crix/candles/'
@@ -15,69 +19,69 @@ def get_coin_data_url(coinName, type, scale, cnt=400) :
 
 # 수정 변수들
 coinName = "XRP"
-to_date = '2021-05-09'
+to_date = '2021-05-01'
 num = 30
 scale = '1'
+if num == 0:
+    type = 'days'
+else:
+    type = 'minutes/{}'.format(num)
+
+basic_url = get_coin_data_url(coinName,type,scale)
+url = basic_url
 
 # get_coin_data_url(coinName,type,scale)
 
-addr = 'https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/30/?code=CRIX.UPBIT.KRW-SSX&count='+str(400)
+# respone = requests.get(url)
 
-
-respone = requests.get(addr)
-
-data = respone.json()
+# data = respone.json()
 # print(data)
-for i in range(len(data)):
-    # dateKst = data[i]['candleDateTime']
-    print("%.2f"%data[i]['openingPrice'])
-    # print(dateKst)
+
     
+# df_for = pd.DataFrame(data)
+# df.append(df_for,ignore_index=True)
+# print(data[0])
 
 
+def get_upbit_data(url, last_date, to_date) :
+    global df
+
+    fail2GetData = False
+    response = ''
+    while True:
+        try :
+            response = requests.get(url)
+        except Exception as e:
+            print(e)
+            time.sleep(20)
+            continue
+        if str(response) == '<Response [200]>':
+            break
+        time.sleep(10)
+    if ( fail2GetData )  :
+        print("Fail to access url")
+        exit()
+
+    data = response.json()
+    df_for = pd.DataFrame(data)
+    df.append(df_for,ignore_index=True)
+    date = ''
+    # print(data[0]['candleDateTime'])
+    date = data[len(data)-1]['candleDateTime']
+    print('Doing well...')
+
+    return date
 
 
-# def get_upbit_data(url, last_date, to_date) :
-#     global show_title
-#     global index
-    
-#     fail2GetData = False
-#     failCnt = 0
-#     response = ''
-#     while True:
-#         try :
-#             response = requests.get(url)
-#         except Exception as e:
-#             print(e)
-#             time.sleep(20)
-#             continue
-#         if str(response) == '<Response [200]>':
-#             break
-#         time.sleep(10)
-#     if ( fail2GetData )  :
-#         print("Fail to access url")
-#         exit()
+while (1) :
+    last_date = get_upbit_data(url, last_date, to_date)
+    tmp1 = last_date.split('T')
+    if tmp1[0]  < to_date :
+        break
+    tmp2 = tmp1[1].split('+')
+    target_date = tmp1[0] + ' ' + tmp2[0]
+    url = basic_url + '&to=' + target_date    #to=2019-11-27 04:01:00
+    time.sleep(2)
 
-#     data = response.json()
 
-#     code = data[0]['code']
-#     date = ''
-#     dateKst = ''
-
-#     for i in range(len(data))  :
-#         dateKst = data[i]['candleDateTimeKst']
-#         date = data[i]['candleDateTime']
-#         if (last_date == '' or last_date > date) :
-#             if (dateKst >= to_date) :
-#                 simpleDate = dateKst.split('+')
-#                 print(simpleDate[0], "%6d"%data[i]['openingPrice'], "%6d"%data[i]['highPrice'], "%6d"%data[i]['lowPrice'], "%6d"%data[i]['tradePrice'], "%9d"%(data[i]['candleAccTradeVolume']));
-#                 datas = (simpleDate[0], "%6f"%data[i]['openingPrice'], "%6f"%data[i]['highPrice'], "%6f"%data[i]['lowPrice'], "%6f"%data[i]['tradePrice'], "%9f"%(data[i]['candleAccTradeVolume']))
-#                 for seq, name in enumerate(datas):
-#                     sheet.cell(row=index, column=seq+1, value=name)
-
-#             else :
-#                 break
-#         index+=1
-#         # print('still working...')
-
-#     return date
+print(df)
